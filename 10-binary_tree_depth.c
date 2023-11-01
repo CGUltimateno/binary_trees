0de906719ef093905816f4e0daa79c1012f6bb85
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-01 08:42:09 +0200

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,6 +8,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
     if (tree == NULL || tree->parent == NULL)
         return (0);
-    size_t depth = binary_tree_depth(tree->parent);
-    return (1 + depth);
+    return (1 + binary_tree_depth(tree->parent));
 }
